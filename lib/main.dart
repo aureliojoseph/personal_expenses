31acd42b719e7c5078a0285d17e848cfd7f529bd
Aurelio Borges <aurelioaborges@gmail.com>
2025-03-31 16:42:02 -0300

    feat: add sample transactions to MyHomePage for initial display

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:personal_expenses/widgets/graph_widget.dart';
 import 'package:personal_expenses/widgets/transaction_list_widget.dart';
 
+import 'models/transaction.dart';
+
 void main() {
   runApp(const PersonalExpenses());
 }
@@ -16,13 +18,29 @@ class PersonalExpenses extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Personal Expenses')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [const GraphWidget(), const TransactionListWidget()],
       ),
